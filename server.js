968bbfa06971fec1d056324f007027880109047a
ToniTsey <113501444+ATE826@users.noreply.github.com>
2025-02-17 13:07:47 +0300

    Шестой коммит

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Error reading the file');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);
+      }
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>404 Not Found</h1>');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
