c556d7dd4c15ad0f79264659e469e49a2db75dd1
ToniTsey <113501444+ATE826@users.noreply.github.com>
2025-02-17 13:50:49 +0300

    Седьмой коммит

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
+  // Обработка запроса на index.html
   if (req.url === '/') {
     fs.readFile(path.join(__dirname, 'index.html'), 'utf-8', (err, data) => {
       if (err) {
@@ -14,10 +15,33 @@ const server = http.createServer((req, res) => {
         res.end(data);
       }
     });
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>404 Not Found</h1>');
+  }
+  // Обработка запроса на CSS файл
+  else if (req.url === '/dist/styles.css') { // Здесь нужно использовать правильный путь
+    fs.readFile(path.join(__dirname, 'dist', 'styles.css'), 'utf-8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Error reading the CSS file');
+      } else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/css');
+        res.end(data);
+      }
+    });
+  }
+  // Обработка ошибки 404
+  else {
+    // Отправка страницы с ошибкой 404
+    fs.readFile(path.join(__dirname, '404.html'), 'utf-8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Error reading the 404 page');
+      } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(data);  // Отправляем страницу с ошибкой 404
+      }
+    });
   }
 });
 
